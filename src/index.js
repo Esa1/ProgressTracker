66eefc2dc84706f55f341ba403a645efbb17b83d
Esa Torkkila <esa.torkkila@gmail.com>
2022-11-29 14:29:10 +0200

    Basic structure created

--- src/index.js
+++ src/index.js
@@ -2,16 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import Styled from "styled-components"
+
+const Section = Styled.section`
+    background-color: #ffffff;
+    border: solid 3px grey;
+    margin: 4px;
+    width: 300px;
+    height: 50px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: left;
+    font-size: 40px;
+    color: #ff6961;
+`
+const WeekdayTitle = Styled.div`
+    display: flex;
+    justify-content: center;
+    width: 50px;
+    border-right: solid 3px lightgrey;
+`
+
+class Main extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Progress Tracker</h1>
+        <div>
+          <Section>
+            <WeekdayTitle>M</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>T</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>W</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>T</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>F</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>S</WeekdayTitle>
+          </Section>
+          <Section>
+            <WeekdayTitle>S</WeekdayTitle>
+          </Section>
+        </div>
+      </>
+    );
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//ReactDOM.render(<Main />, document.getElementById('root'))
